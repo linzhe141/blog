837a757858762e4e5b0e7d692242dba66fdf4445
linzhe <linzhe141@qq.com>
2023-08-21 17:10:31 +0800

    [feat] 新增webapp前端代码

---
+++ docker/nginx-app/webapp/README.md
@@ -0,0 +1,1 @@
+# 一个简单的 webapp，用于学习 docker
---
+++ docker/nginx-app/webapp/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted, ref } from "vue";
+const list = ref([]);
+onMounted(async () => {
+  list.value = await (await fetch("/api/list")).json();
+});
+</script>
+
+<template>
+  <h1>一个简单的 webapp，用于学习 docker</h1>
+  <ul>
+    <li v-for="item of list.data" :key="item.value">{{ item.name }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
