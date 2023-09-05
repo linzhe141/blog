8a7b28506513b7e7b239d3d05a11478e1445e210
linzhe141 <linzhe141@qq.com>
2023-09-05 23:00:16 +0800

    [feat] 首页新增logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
 'use client'
 import Underline from '@/components/underline'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 import { useStore } from '@/store/store'
 import { NavData } from '@/components/layout/types'
 export default function Home() {
   const router = useRouter()
   const navList = useStore((state) => state.navList)
-  // function getDefaultUrl(data: NavData[]) {
-  //   if (data.length) {
-  //     const target = data[0]
-  //     if (!target.children || target.children?.length === 0) return target.url
-  //     if (target.children) {
-  //       return getDefaultUrl(target.children)
-  //     }
-  //   }
-  //   return null
-  // }
   function getDefaultUrl(data: NavData[]) {
     if (data.length) {
       const target = data[0]
@@ -27,12 +18,16 @@ export default function Home() {
     <div className='flex h-screen items-center justify-center bg-green-400'>
       <div className='flex flex-col items-center justify-center'>
         <Underline height={8}>
-          <h1
-            className='mb-4 cursor-pointer text-9xl'
-            onClick={() => router.push(getDefaultUrl(navList) ?? '')}
-          >
-            blog
-          </h1>
+          <div onClick={() => router.push(getDefaultUrl(navList) ?? '')} className='flex flex-col justify-center items-center'>
+            <Image
+              className='mr-2 rounded-full'
+              width={120}
+              height={120}
+              src={'/logo.jpg'}
+              alt='logo'
+            />
+            <h1 className='mb-4 cursor-pointer text-9xl'>blog</h1>
+          </div>
         </Underline>
       </div>
     </div>
