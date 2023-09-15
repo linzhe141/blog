33e3200330a57c744e3f6ef7ddbab1754509fab5
linzhe <linzhe141@qq.com>
2023-09-15 17:52:09 +0800

    [test] 取消api/nav的post

--- app/api/nav/route.ts
+++ app/api/nav/route.ts
@@ -2,7 +2,7 @@ import fs from 'fs-extra'
 import path from 'path'
 import { NextResponse } from 'next/server'
 import { prisma } from '@/prisma'
-// import { headers } from 'next/headers'
+import { headers } from 'next/headers'
 
 const blogDirName = 'blog'
 async function getBlogUrlList(
@@ -125,27 +125,27 @@ export async function GET(request: Request) {
   return NextResponse.json({ data: blogUrlList })
 }
 
-export async function POST(request: Request) {
-  // const headersList = headers()
-  // const requestKey = headersList.get('authorization') ?? ''
-  // const target = await prisma.requsetKey.findFirst({
-  //   where: {
-  //     key: requestKey,
-  //   },
-  // })
-  // if (!target) {
-  //   return NextResponse.json({ code: 401, msg: '认证失败！' })
-  // }
-  const { data } = await request.json()
-  await prisma.$transaction(
-    data.map((item: { id: number; label: string }) =>
-      prisma.menu.update({
-        where: { id: item.id },
-        data: {
-          label: item.label,
-        },
-      })
-    )
-  )
-  return NextResponse.json({ data: true })
-}
+// export async function POST(request: Request) {
+//   const headersList = headers()
+//   const requestKey = headersList.get('authorization') ?? ''
+//   const target = await prisma.requsetKey.findFirst({
+//     where: {
+//       key: requestKey,
+//     },
+//   })
+//   if (!target) {
+//     return NextResponse.json({ code: 401, msg: '认证失败！' })
+//   }
+//   const { data } = await request.json()
+//   await prisma.$transaction(
+//     data.map((item: { id: number; label: string }) =>
+//       prisma.menu.update({
+//         where: { id: item.id },
+//         data: {
+//           label: item.label,
+//         },
+//       })
+//     )
+//   )
+//   return NextResponse.json({ data: true })
+// }
