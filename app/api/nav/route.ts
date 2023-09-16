8bcf9df02e065cf874cfdead0f437b4300a59a3c
linzhe141 <linzhe141@qq.com>
2023-09-16 10:25:06 +0800

    [test]

--- app/api/nav/route.ts
+++ app/api/nav/route.ts
@@ -122,34 +122,34 @@ export async function GET(request: Request) {
         id: 'asc',
       },
     })
-    return NextResponse.json({ data: formatMenu(menuList) })
+    return NextResponse.json({ data: formatMenu(menuList), xx: usePrisma })
   } else {
-    return NextResponse.json({ data: blogUrlList })
+    return NextResponse.json({ data: blogUrlList, yy: usePrisma })
   }
 }
 
-// export async function POST(request: Request) {
-//   // const headersList = headers()
-//   // const requestKey = headersList.get('authorization')!
-//   // const target = await prisma.requsetKey.findFirst({
-//   //   where: {
-//   //     key: requestKey,
-//   //   },
-//   // })
-//   // if (!target) {
-//   //   return NextResponse.json({ code: 401, msg: '认证失败！' })
-//   // }
-//   // const { data } = await request.json()
-//   // await prisma.$transaction(
-//   //   data.map((item: { id: number; label: string }) =>
-//   //     prisma.menu.update({
-//   //       where: { id: item.id },
-//   //       data: {
-//   //         label: item.label,
-//   //       },
-//   //     })
-//   //   )
-//   // )
-//   // return NextResponse.json({ data: true })
-//   return NextResponse.json({ data: true })
-// }
+export async function POST(request: Request) {
+  // const headersList = headers()
+  // const requestKey = headersList.get('authorization')!
+  // const target = await prisma.requsetKey.findFirst({
+  //   where: {
+  //     key: requestKey,
+  //   },
+  // })
+  // if (!target) {
+  //   return NextResponse.json({ code: 401, msg: '认证失败！' })
+  // }
+  // const { data } = await request.json()
+  // await prisma.$transaction(
+  //   data.map((item: { id: number; label: string }) =>
+  //     prisma.menu.update({
+  //       where: { id: item.id },
+  //       data: {
+  //         label: item.label,
+  //       },
+  //     })
+  //   )
+  // )
+  // return NextResponse.json({ data: true })
+  return NextResponse.json({ data: true })
+}
