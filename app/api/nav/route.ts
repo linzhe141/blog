e32ceb0217a0619c83c79d73c5ca3eeebd571067
linzhe141 <linzhe141@qq.com>
2023-09-16 09:43:42 +0800

    [test] 验证post是否影响get的响应结果

--- app/api/nav/route.ts
+++ app/api/nav/route.ts
@@ -128,28 +128,28 @@ export async function GET(request: Request) {
   }
 }
 
-export async function POST(request: Request) {
-  // const headersList = headers()
-  // const requestKey = headersList.get('authorization')!
-  // const target = await prisma.requsetKey.findFirst({
-  //   where: {
-  //     key: requestKey,
-  //   },
-  // })
-  // if (!target) {
-  //   return NextResponse.json({ code: 401, msg: '认证失败！' })
-  // }
-  // const { data } = await request.json()
-  // await prisma.$transaction(
-  //   data.map((item: { id: number; label: string }) =>
-  //     prisma.menu.update({
-  //       where: { id: item.id },
-  //       data: {
-  //         label: item.label,
-  //       },
-  //     })
-  //   )
-  // )
-  // return NextResponse.json({ data: true })
-  return NextResponse.json({ data: true })
-}
+// export async function POST(request: Request) {
+//   // const headersList = headers()
+//   // const requestKey = headersList.get('authorization')!
+//   // const target = await prisma.requsetKey.findFirst({
+//   //   where: {
+//   //     key: requestKey,
+//   //   },
+//   // })
+//   // if (!target) {
+//   //   return NextResponse.json({ code: 401, msg: '认证失败！' })
+//   // }
+//   // const { data } = await request.json()
+//   // await prisma.$transaction(
+//   //   data.map((item: { id: number; label: string }) =>
+//   //     prisma.menu.update({
+//   //       where: { id: item.id },
+//   //       data: {
+//   //         label: item.label,
+//   //       },
+//   //     })
+//   //   )
+//   // )
+//   // return NextResponse.json({ data: true })
+//   return NextResponse.json({ data: true })
+// }
