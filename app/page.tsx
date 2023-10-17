aecab543a28d07699efca3f00d9a3a445b0856c7
linzhe <linzhe141@qq.com>
2023-10-17 10:35:30 +0800

    [feat] 首页新增无限抛光动画效果

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,14 @@ export default function Home() {
   return (
     <div className='flex h-screen items-center justify-center bg-green-400 '>
       <div className='flex flex-col items-center justify-center'>
-        <Underline height={8}>
-          <Link
-            href={getDefaultUrl(navList) ?? ''}
-            className='flex flex-col items-center justify-center'
-          >
-            <h1 className='mb-4 text-9xl'>blog</h1>
-            <span className='text-xl'>好记性不如烂笔头</span>
-          </Link>
-        </Underline>
+        <Link
+          href={getDefaultUrl(navList) ?? ''}
+          className='flex flex-col items-center justify-center'
+        >
+          <h1 className='mb-4 text-9xl'>blog</h1>
+          <span className='text-xl'>好记性不如烂笔头</span>
+          <div className={`gradient mt-2 h-2 w-[400px]`}></div>
+        </Link>
       </div>
     </div>
   )
