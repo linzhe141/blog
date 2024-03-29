b43f778552e5b056a4a2aa9ba5fe6993c8381c08
linzhe141 <linzhe141@qq.com>
2024-03-29 18:02:18 +0800

    wip: 新增基础结构

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -1,50 +1,369 @@
 'use client'
-import { useState, useEffect } from 'react'
-import Auth from './auth'
-import MenuSetting from './menuSetting'
-import { type Result } from '@/types'
-import Skeleton from 'react-loading-skeleton'
-export default function Setting() {
-  const [auth, setAuth] = useState(false)
-  const [loading, setLoading] = useState(true)
-  async function checkStatus() {
-    setLoading(true)
-    const data: Result<boolean> = await (
-      await fetch('/api/requestKey', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          key: localStorage.getItem('requestKey') ?? 'null',
-        }),
-      })
-    ).json()
-    setLoading(false)
-    setAuth(data.data)
+import React, { useEffect, useRef, useState } from 'react'
+import { useImmer } from 'use-immer'
+import {
+  DndContext,
+  closestCenter,
+  DragOverlay,
+  DragStartEvent,
+  DragOverEvent,
+  DragEndEvent,
+  DragMoveEvent,
+  Active,
+  Over,
+} from '@dnd-kit/core'
+import {
+  AnimateLayoutChanges,
+  SortableContext,
+  arrayMove,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import {
+  restrictToVerticalAxis,
+  restrictToWindowEdges,
+} from '@dnd-kit/modifiers'
+import Container from './Container'
+import Icon from '@/components/icon/Icon'
+function formatData(data: any[]) {
+  const stack: any[] = []
+  function foo(data: any[], level = 1) {
+    data.forEach((i, index) => {
+      i.isDragging = false
+      i.level = level
+      i.paddingLeft = 0
+      i.isLastChild = index === data.length - 1
+      i.forward = 0
+      if (stack.length) {
+        const prev = stack[stack.length - 1]
+        i.backward = prev.level - i.level + 1
+      } else {
+        i.backward = 0
+      }
+      stack.push(i)
+      i.children = i.children ?? []
+      if (i.children && i.children.length) {
+        foo(i.children, level + 1)
+      }
+    })
+    return data
+  }
+  const result = foo(data)
+  for (let i = 0; i < stack.length - 1; i++) {
+    const item = stack[i]
+    if (item.isLastChild) {
+      for (let j = i + 1; j < stack.length; j++) {
+        const next = stack[j]
+        if (next.level < item.level) {
+          item.forward = item.level - next.level
+          break
+        }
+      }
+    }
+  }
+  return result
+}
+
+export default function App() {
+  const [menuData, setMenuData] = useImmer(
+    formatData([
+      { id: '1', label: 'test1' },
+      {
+        id: '3',
+        label: 'test3',
+        children: [
+          {
+            id: '3-1',
+            label: 'test3-1',
+            children: [{ id: '3-1-1', label: 'test3-1-1' }],
+          },
+          { id: '3-2', label: 'test3-2' },
+          {
+            id: '3-3',
+            label: 'test3-3',
+            children: [{ id: '3-3-1', label: 'test3-3-1' }],
+          },
+        ],
+      },
+      { id: '4', label: 'test4' },
+    ])
+  )
+  const [activeNode, setActiveNode] = useState<any>(null)
+  const handleDragStart = ({ active }: DragStartEvent) => {
+    setActiveNode(getNodeByID(menuData, active.id as string))
+    setMenuData((draft) => {
+      function foo(data: any[]) {
+        for (const item of data) {
+          item.isDragging = item.id === active.id
+          if (item.children && item.children.length) {
+            foo(item.children)
+          }
+        }
+      }
+      foo(draft)
+    })
+  }
+  const handleDragCancel = () => setActiveNode(null)
+  const [offsetLeft, setOffsetLeft] = useState(0)
+  function handleDragMove({ delta, active, over }: DragMoveEvent) {}
+  const handleDragOver = ({ active, over }: DragOverEvent) => {
+    const overId = over?.id
+    if (!overId) {
+      return
+    }
+    // setMenuData((draft) => {
+    //   remove(draft, active.id as string)
+    // })
   }
 
+  function handleDragEnd({ active, over, delta }: DragEndEvent) {
+    if (!over || over.id === 'tree-container') {
+      setActiveNode(null)
+      return
+    }
+    const distanceX = delta.x
+    const overTop = over.rect.top
+    const activeTop = active.rect.current.initial!.top
+    const direction: Direction = activeTop >= overTop ? 'up' : 'down'
+    if (active.id !== over.id && distanceX <= 20 && distanceX >= -20) {
+      setMenuData((draft) => {
+        const activeNode = getNodeByID(draft, active.id as string)
+        const overNode = getNodeByID(draft, over.id as string)
+        remove(draft, active.id as string)
+        if (overNode.children.length && direction === 'down') {
+          overNode.children.unshift(activeNode)
+        } else {
+          insert(direction, draft, over.id as string, activeNode)
+        }
+        formatData(draft)
+      })
+    }
+    // const overNode = getNodeByID(menuData, over.id as string)
+    // for (let i = 1; i <= overNode.backward; i++) {
+    //   if (distanceX > 20 * i) {
+    //     setMenuData((draft) => {
+    //       const activeNode = getNodeByID(draft, active.id as string)
+    //       let parent = getParentNodeByID(draft, over.id as string) || {
+    //         children: draft,
+    //       }
+    //       const targetIndex =
+    //         parent.children.findIndex((i: any) => i.id === over.id) - 1
+    //       const targetNode = parent.children[targetIndex]
+    //       remove(draft, active.id as string)
+    //       targetNode.children.push(activeNode)
+    //       formatData(draft)
+    //     })
+    //   }
+    // }
+    setActiveNode(null)
+    setMenuData((draft) => {
+      formatData(draft)
+    })
+  }
+  const [isMounted, setIsMounted] = useState(false)
+
   useEffect(() => {
-    checkStatus()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setIsMounted(true)
   }, [])
 
+  if (!isMounted) return null
   return (
-    <div>
-      {loading ? (
-        <div className='flex h-screen items-center justify-center'>
-          <div className='w-[400px]'>
-            <Skeleton height={40} count={10} />
+    <DndContext
+      id='builder-dnd'
+      // modifiers={[restrictToVerticalAxis]}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragCancel={handleDragCancel}
+      onDragOver={handleDragOver}
+      onDragMove={handleDragMove}
+      onDragEnd={handleDragEnd}
+    >
+      <div className='mx-4 mt-4 w-1/4'>
+        {/* <Container
+          id='tree-container'
+          items={getIds(menuData)}
+          data={menuData}
+        /> */}
+        <SortableContext
+          id='tree-container'
+          items={getIds(menuData)}
+          strategy={verticalListSortingStrategy}
+        >
+          {menuData.map((i) => (
+            <SortableTreeItem data={menuData} node={i} key={i.id} />
+          ))}
+        </SortableContext>
+      </div>
+      <DragOverlay>
+        {activeNode ? (
+          <div
+            // style={{ marginLeft: 20 * (activeNode.level - 1) + 'px' }}
+            className='mb-1 flex h-10 w-60 items-center border-2 border-dashed border-green-400 bg-green-200 px-2 opacity-70'
+          >
+            <span className='mr-2 h-4 w-4 cursor-all-scroll'>
+              <Icon type='rank' />
+            </span>
+            <span>{activeNode.label}</span>
           </div>
+        ) : // <div>xxx</div>
+        null}
+      </DragOverlay>
+    </DndContext>
+  )
+}
+function getIds(data: any[], result = []) {
+  for (const item of data) {
+    // @ts-expect-error
+    result.push(item.id)
+    if (item.children && item.children.length) {
+      getIds(item.children, result)
+    }
+  }
+  return result
+}
+function remove(data: any[], id: string) {
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i]
+    if (item.id === id) {
+      data.splice(i, 1)
+    }
+    if (item.children && item.children.length) {
+      remove(item.children, id)
+    }
+  }
+}
+function getNodeByID(data: any[], id: string): any | null {
+  for (const item of data) {
+    if (item.id === id) {
+      return item
+    }
+    if (item.children && item.children.length) {
+      const target = getNodeByID(item.children, id)
+      if (target) {
+        return target
+      }
+    }
+  }
+  return null
+}
+type Direction = 'up' | 'down'
+function insert(direction: Direction, data: any[], id: string, value: any) {
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i]
+    if (item.id === id) {
+      const index = direction === 'up' ? 0 : 1
+      data.splice(i + index, 0, value)
+      return
+    }
+    if (item.children && item.children.length) {
+      insert(direction, item.children, id, value)
+    }
+  }
+}
+
+function getParentNodeByID(data: any[], id: string): any | null {
+  for (const item of data) {
+    if (item.children && item.children.length) {
+      if (item.children.find((i: any) => i.id === id)) {
+        return item
+      }
+      const target = getParentNodeByID(item.children, id)
+      if (target) {
+        return target
+      }
+    }
+  }
+  return null
+}
+
+import { CSS } from '@dnd-kit/utilities'
+import { cn } from '@/utils'
+const animateLayoutChanges: AnimateLayoutChanges = ({
+  isSorting,
+  wasDragging,
+}) => (isSorting || wasDragging ? false : true)
+function SortableTreeItem({ node, data }: any) {
+  const { level, id } = node
+  const {
+    attributes,
+    isDragging,
+    isSorting,
+    listeners,
+    setDraggableNodeRef,
+    setDroppableNodeRef,
+    transform,
+    transition,
+    active,
+    over,
+  } = useSortable({
+    id,
+    animateLayoutChanges,
+  })
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+  }
+  const currentPadding = useRef(20 * (level - 1))
+
+  const paddingLeft = useRef(0)
+  if (
+    over &&
+    over.id !== 'tree-container' &&
+    active &&
+    active.rect.current.translated &&
+    active.rect.current.initial &&
+    active.id === id
+  ) {
+    console.log(over.id)
+
+    const distanceX =
+      active.rect.current.translated.left - active.rect.current.initial.left
+    const overNode = getNodeByID(data, over.id as string)
+    const activeNode = getNodeByID(data, active.id as string)
+    currentPadding.current =
+      20 * (Math.min(overNode.level, activeNode.level) - 1)
+    for (let i = 0; i <= overNode.backward; i++) {
+      if (distanceX > 20 * i) {
+        paddingLeft.current = 20 * i
+        console.log(paddingLeft.current)
+      }
+    }
+    for (let i = 0; i <= overNode.forward; i++) {
+      if (distanceX < -20 * i) {
+        paddingLeft.current = -20 * i
+        console.log(paddingLeft.current)
+      }
+    }
+  }
+  return (
+    <>
+      <div
+        ref={setDroppableNodeRef}
+        style={{
+          paddingLeft: currentPadding.current + paddingLeft.current + 'px',
+        }}
+      >
+        <div
+          style={{ ...style }}
+          className={cn('mb-3 flex h-10 items-center px-2', {
+            'bg-blue-200': isDragging,
+            'bg-red-200': !isDragging,
+          })}
+          ref={setDraggableNodeRef}
+        >
+          <div className='h-4 w-4' {...attributes} {...listeners}>
+            <Icon type='rank'></Icon>
+          </div>
+          <span>
+            {node.label}-level:{node.level}-backward: {node.backward}
+            -forward: {node.forward}
+          </span>
         </div>
-      ) : !auth ? (
-        <div className='flex h-screen items-center justify-center'>
-          <Auth setAuth={setAuth} />
-        </div>
-      ) : (
-        <MenuSetting setAuth={setAuth} />
-      )}
-      {/* {!auth ? <Auth setAuth={setAuth} /> : <MenuSetting setAuth={setAuth} />} */}
-    </div>
+      </div>
+      {!node.isDragging &&
+        node.children.map((i: any) => (
+          <SortableTreeItem data={data} node={i} key={i.id} />
+        ))}
+    </>
   )
 }
