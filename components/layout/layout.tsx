7e721eaa5e7982aebdbff962bd6739050ec8f3c9
linzhe <linzhe141@qq.com>
2024-06-20 17:16:06 +0800

    chore: 优化SkeletonTheme刷新时的颜色异常问题

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -17,11 +17,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     init()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  let baseColor = 'transparent'
+  let highlightColor = 'transparent'
+  if (mode === 'dark') {
+    baseColor = '#202020'
+    highlightColor = '#444'
+  }
+  if (mode === 'light') {
+    baseColor = ''
+    highlightColor = ''
+  }
   return (
-    <SkeletonTheme
-      baseColor={mode === 'dark' ? '#202020' : ''}
-      highlightColor={mode === 'dark' ? '#444' : ''}
-    >
+    <SkeletonTheme baseColor={baseColor} highlightColor={highlightColor}>
       <div className='dark:bg-black'>
         {children}
         <Next13ProgressBar
