3f68cd2a8f98370fdd505d91507b29f7e7ded6af
linzhe <linzhe141@qq.com>
2025-04-10 22:44:42 +0800

    fix: downgrade remark-gfm to version 3.0.1 and update related dependencies

--- app/readme/page.tsx
+++ app/readme/page.tsx
@@ -1,9 +1,7 @@
 import fs from 'fs-extra'
 import path from 'path'
-import matter from 'gray-matter'
 import MdxDisplayRsc from '@/components/mdx/mdxDisplayRsc'
 import CursorFollow from '@/components/cursorFollow'
-import { convertMd2Html } from '@/lib/utils'
 export default async function Page() {
   const { content } = await getReadmeContent()
   return (
@@ -17,8 +15,7 @@ async function getReadmeContent() {
   const cwd = process.cwd()
   const result = path.resolve(cwd, `readme.md`)
   const source = await fs.readFile(result, 'utf-8')
-  const { content } = matter(source)
   return {
-    content: convertMd2Html(content),
+    content: source,
   }
 }
