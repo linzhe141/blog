15173aab88a9b881e1b62b98ff0b1c782137bd34
linzhe141 <1572213544@qq.com>
2025-05-09 17:40:23 +0800

    refactor: restructure NotFound component layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,28 @@
 'use client'
 import Header from '@/components/layout/header'
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 export default function NotFound() {
   const router = useRouter()
   return (
     <div className='flex flex-col items-center justify-center'>
       <Header></Header>
-      <Image
-        className='mt-20'
-        src='/404.svg'
-        alt='404'
-        width={550}
-        height={470}
-      />
-      <Button onClick={() => router.back()} variant='outline' className='mb-4'>
-        返回
-      </Button>
+      <div className='h-screen'>
+        <div className='mt-[200px] space-x-20 text-5xl text-black dark:text-white'>
+          <span>4</span>
+          <span>0</span>
+          <span>4</span>
+        </div>
+        <div className='mt-10 text-center'>
+          <Button
+            onClick={() => router.back()}
+            variant='outline'
+            className='mb-4'
+          >
+            <span className='mr-8'>返</span>回
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
